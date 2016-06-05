659561d8b1bb1ba14c9c4e2eab5bc229eb26c6c4
mohammadsaleh <mohammadsaleh.sayari@gmail.com>
2016-06-05 19:59:38 +0430

    acl

--- docs/spider.sql
+++ docs/spider.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2016-06-04 11:35:57
+Date: 2016-06-04 20:12:29
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -86,7 +86,7 @@ CREATE TABLE `spider_aclmanager_aros` (
   `model` varchar(255) DEFAULT NULL,
   `foreign_key` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of spider_aclmanager_aros
@@ -96,6 +96,10 @@ INSERT INTO `spider_aclmanager_aros` VALUES ('28', 'users', '1');
 INSERT INTO `spider_aclmanager_aros` VALUES ('29', 'roles', '4');
 INSERT INTO `spider_aclmanager_aros` VALUES ('30', 'roles', '1');
 INSERT INTO `spider_aclmanager_aros` VALUES ('31', 'roles', '3');
+INSERT INTO `spider_aclmanager_aros` VALUES ('32', 'users', '2');
+INSERT INTO `spider_aclmanager_aros` VALUES ('33', 'users', '36');
+INSERT INTO `spider_aclmanager_aros` VALUES ('34', 'users', '6');
+INSERT INTO `spider_aclmanager_aros` VALUES ('35', 'users', '3');
 
 -- ----------------------------
 -- Table structure for spider_aclmanager_aros_acos
@@ -108,7 +112,7 @@ CREATE TABLE `spider_aclmanager_aros_acos` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `aro_id` (`aro_id`,`aco_id`),
   KEY `aco_id` (`aco_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=589 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1316 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of spider_aclmanager_aros_acos
@@ -204,6 +208,63 @@ INSERT INTO `spider_aclmanager_aros_acos` VALUES ('500', '31', '46');
 INSERT INTO `spider_aclmanager_aros_acos` VALUES ('496', '31', '47');
 INSERT INTO `spider_aclmanager_aros_acos` VALUES ('498', '31', '48');
 INSERT INTO `spider_aclmanager_aros_acos` VALUES ('497', '31', '49');
+INSERT INTO `spider_aclmanager_aros_acos` VALUES ('597', '32', '45');
+INSERT INTO `spider_aclmanager_aros_acos` VALUES ('598', '32', '46');
+INSERT INTO `spider_aclmanager_aros_acos` VALUES ('594', '32', '47');
+INSERT INTO `spider_aclmanager_aros_acos` VALUES ('596', '32', '48');
+INSERT INTO `spider_aclmanager_aros_acos` VALUES ('595', '32', '49');
+INSERT INTO `spider_aclmanager_aros_acos` VALUES ('1314', '33', '45');
+INSERT INTO `spider_aclmanager_aros_acos` VALUES ('1315', '33', '46');
+INSERT INTO `spider_aclmanager_aros_acos` VALUES ('1311', '33', '47');
+INSERT INTO `spider_aclmanager_aros_acos` VALUES ('1313', '33', '48');
+INSERT INTO `spider_aclmanager_aros_acos` VALUES ('1312', '33', '49');
+
+-- ----------------------------
+-- Table structure for spider_aclmanager_roles
+-- ----------------------------
+DROP TABLE IF EXISTS `spider_aclmanager_roles`;
+CREATE TABLE `spider_aclmanager_roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `lft` int(11) DEFAULT NULL,
+  `rght` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`),
+  CONSTRAINT `spider_aclmanager_roles_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `spider_aclmanager_roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of spider_aclmanager_roles
+-- ----------------------------
+INSERT INTO `spider_aclmanager_roles` VALUES ('1', '2', 'superadmin', 'مدیریت کل', 'توضیحات', '5', '6');
+INSERT INTO `spider_aclmanager_roles` VALUES ('2', '3', 'registered', 'کاربران ثبت نام شده', 'کاربران ثبت نام شده', '2', '11');
+INSERT INTO `spider_aclmanager_roles` VALUES ('3', null, 'public', 'کاربران عمومی', '', '1', '12');
+INSERT INTO `spider_aclmanager_roles` VALUES ('4', '2', 'admin', 'مدیر', '', '3', '4');
+INSERT INTO `spider_aclmanager_roles` VALUES ('5', '2', 'counter', 'کانتر', null, '7', '10');
+INSERT INTO `spider_aclmanager_roles` VALUES ('6', '5', 'agency', 'آژانس', null, '8', '9');
+
+-- ----------------------------
+-- Table structure for spider_aclmanager_users_roles
+-- ----------------------------
+DROP TABLE IF EXISTS `spider_aclmanager_users_roles`;
+CREATE TABLE `spider_aclmanager_users_roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of spider_aclmanager_users_roles
+-- ----------------------------
+INSERT INTO `spider_aclmanager_users_roles` VALUES ('1', '1', '1');
+INSERT INTO `spider_aclmanager_users_roles` VALUES ('6', '28', '2');
+INSERT INTO `spider_aclmanager_users_roles` VALUES ('7', '33', '2');
+INSERT INTO `spider_aclmanager_users_roles` VALUES ('46', '36', '1');
+INSERT INTO `spider_aclmanager_users_roles` VALUES ('49', '36', '6');
 
 -- ----------------------------
 -- Table structure for spider_plugins_plugins
@@ -272,31 +333,6 @@ CREATE TABLE `spider_users_activation_keys` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for spider_users_roles
--- ----------------------------
-DROP TABLE IF EXISTS `spider_users_roles`;
-CREATE TABLE `spider_users_roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `parent_id` int(11) DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `description` text COLLATE utf8_unicode_ci,
-  `lft` int(11) DEFAULT NULL,
-  `rght` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `parent_id` (`parent_id`),
-  CONSTRAINT `spider_users_roles_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `spider_users_roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- ----------------------------
--- Records of spider_users_roles
--- ----------------------------
-INSERT INTO `spider_users_roles` VALUES ('1', '2', 'superadmin', 'مدیریت کل', 'توضیحات', '5', '6');
-INSERT INTO `spider_users_roles` VALUES ('2', '3', 'registered', 'کاربران ثبت نام شده', 'کاربران ثبت نام شده', '2', '7');
-INSERT INTO `spider_users_roles` VALUES ('3', null, 'public', 'کاربران عمومی', '', '1', '8');
-INSERT INTO `spider_users_roles` VALUES ('4', '2', 'admin', 'مدیر', '', '3', '4');
-
--- ----------------------------
 -- Table structure for spider_users_users
 -- ----------------------------
 DROP TABLE IF EXISTS `spider_users_users`;
@@ -313,25 +349,21 @@ CREATE TABLE `spider_users_users` (
   `status` tinyint(4) DEFAULT '0' COMMENT '-1=حذف شده\r\n0=غیرفعال\r\n1=فعال',
   `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of spider_users_users
 -- ----------------------------
 INSERT INTO `spider_users_users` VALUES ('1', 'admin', '$2y$10$QTZ43cY04mY9YTxEl23QFOkRt9FLFgHO6fCS5FjlLeHv.t/Jt1BgW', 'mohammadsaleh', 'sayari', 'Spider admin', '09151206735', '1366/11/16', '/img/avatars/1/0035211c5f5806b7c65234edeacdfc49.jpg', '1', '2016-06-04 11:33:45');
-
--- ----------------------------
--- Table structure for spider_users_users_roles
--- ----------------------------
-DROP TABLE IF EXISTS `spider_users_users_roles`;
-CREATE TABLE `spider_users_users_roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `role_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of spider_users_users_roles
--- ----------------------------
-INSERT INTO `spider_users_users_roles` VALUES ('1', '1', '1');
+INSERT INTO `spider_users_users` VALUES ('2', 'test', '$2y$10$QTZ43cY04mY9YTxEl23QFOkRt9FLFgHO6fCS5FjlLeHv.t/Jt1BgW', 'm', 's', null, null, null, null, '1', '2016-06-04 13:25:28');
+INSERT INTO `spider_users_users` VALUES ('22', 'ali_s@gmail.com', '$2y$10$Vhd1xu4sRK91NOk8cE6w/eSdv1g07yZrGzW0EVF9ylo.JT/wtztOm', null, null, null, null, null, null, '0', '2016-06-04 15:52:29');
+INSERT INTO `spider_users_users` VALUES ('23', 'ali_secad95a6-7a20-48b7-9301-d5edae2329ed@gmail.com', '$2y$10$qgJE8nnmt.14i6oHNmQV0esQTowDw31fQ3vrECVTxtP2eX8uC3ISS', null, null, null, null, null, null, '0', '2016-06-04 15:53:27');
+INSERT INTO `spider_users_users` VALUES ('24', 'ali_s18d6b483-1933-40a2-aec1-c94f31de56e7@gmail.com', '$2y$10$TSuW1o43CBvR8n6FiSdPYOiwfwEj11mEwXd1PjEW3yLlQAy5bY066', null, null, null, null, null, null, '0', '2016-06-04 15:53:34');
+INSERT INTO `spider_users_users` VALUES ('25', 'ali_sae1461d1-cf34-4ba7-978a-abd37a213c9f@gmail.com', '$2y$10$Gh3tYL4s759IeSSGLVcmvOnDVsmcpk3SZgfMqhyuZVaK/d29cGk3q', null, null, null, null, null, null, '0', '2016-06-04 15:53:45');
+INSERT INTO `spider_users_users` VALUES ('26', 'ali_sfa3149c0-99e1-4efa-81ac-2c86f184e9e9@gmail.com', '$2y$10$.FhXFy8Zm5mY/nXRctFKRu6jcpCx8IDhxDkktdkkI8Lrg3s.zN.m.', null, null, null, null, null, null, '0', '2016-06-04 16:25:18');
+INSERT INTO `spider_users_users` VALUES ('27', '2c86f184e9e9@gmail.com', '$2y$10$w6lDRjRfpZHBGC1uvUMw8OkPve68guRKPui1xuQFrNMp6FqhEqtqO', null, null, null, null, null, null, '0', '2016-06-04 16:25:58');
+INSERT INTO `spider_users_users` VALUES ('28', 'ali_sea96a84b-40a8-4a96-8cc2-d03de4e7f36f@gmail.com', '$2y$10$FSZBl/lrar5xrD1sOh5IBe2SVU612Uu9LID4l2tiU5V814rAZ/qM.', null, null, null, null, null, null, '0', '2016-06-04 17:41:13');
+INSERT INTO `spider_users_users` VALUES ('33', 'ali_sc0450f77-5ecb-4c45-851a-2fa71ecd5731@gmail.com', '$2y$10$1xmgNF14pPEpuzHKtj9G/e/Rh2vJCnOTd9vs05BvKHNnrZ/ldfPr6', null, null, null, null, null, null, '0', '2016-06-04 18:07:52');
+INSERT INTO `spider_users_users` VALUES ('34', 'ali_scb5a8d60-62fd-4fb8-9f51-ccbf435cdbe9@gmail.com', '$2y$10$HCLGxgDgz45mHDWd9wIG9eJhILZYGAsWeIkCXt7eQzJQYRUXibjXa', null, null, null, null, null, null, '0', '2016-06-04 18:16:00');
+INSERT INTO `spider_users_users` VALUES ('35', 'ali_s1563a57b-5c66-4f91-a778-94bee71a9b84@gmail.com', '$2y$10$A2yLqQbfLPtEEsprAAhsA.TqnlJd0q7a0.KRQocXxBNPwupBMp73W', null, null, null, null, null, null, '0', '2016-06-04 18:22:11');
+INSERT INTO `spider_users_users` VALUES ('36', 'ali_s21a9f89d-deda-41ff-b68e-33bbc0a8f389@gmail.com', '$2y$10$23gZCTqFpNixq6KRGDEsSuhgnmGCeA36LuaDqqv4VdATGA5mM.jDm', null, null, null, null, null, null, '0', '2016-06-04 19:11:12');
